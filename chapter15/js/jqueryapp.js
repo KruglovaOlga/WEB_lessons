11b1b8a52ceee725cad2333320a4f35bc665f4be
KruglovaOlga <std094996@ac.eap.gr>
2023-02-16 21:06:40 +0200

    jquery

---
+++ chapter15/js/jqueryapp.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+    sayHello()
+    sayLorem()
+    sayHelloCF()
+})
+
+function sayHello() {
+    $('h1').html('Hello CF')
+}
+
+function sayLorem() {
+    //const p = $('<p></p>')
+    const p = $(document.createElement('p'))
+    p.html('Lorem ipsum, dolor ....')
+    $('body').append(p)
+}
+
+function sayHelloCF() {
+    $('<div class="container">').appendTo('body')
+    $('<p class="cf-text"></p>').appendTo('.container')
+    $('.cf-text').html('Hello Coding Factory')
+    $('cf-text').css('color', 'red')
+    $('.container').css(
+        {'background-color' : 'blue',
+        'border': '2px solid green',
+        'text-align' : 'center'}
+        )
+}
